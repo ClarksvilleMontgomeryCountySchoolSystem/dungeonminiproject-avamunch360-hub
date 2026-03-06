a5bd698e8a91a4b784ba00bc375efcaa8b97a3a4
avamunch360-hub <ava.munch360@students.cmcss.net>
2026-03-06 07:31:07 -0600

    Update task4.py

---
+++ task4.py
@@ -0,0 +1,7 @@
+guard_alert =  False
+
+if not gaurd_alert:
+    outcome =  "Silent you slip past the gaurd unnoticed."
+else:
+    outcome = "Doom: The guard spots you and raises the alarm."
+print(outcome)
