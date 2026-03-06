d5ed4a699bf280d70495e3dcb6d8c28fe09d5992
avamunch360-hub <ava.munch360@students.cmcss.net>
2026-03-06 07:28:52 -0600

    Update task2.py

---
+++ task2.py
@@ -0,0 +1,7 @@
+has_key = True
+
+if has_key:
+    Outcome = "Click: The lock turns smoothly and the door creaks open."
+else:
+    Outcome = "Doom: The door stays sealed as footsteps echo behind you. "
+print(Outcome)
