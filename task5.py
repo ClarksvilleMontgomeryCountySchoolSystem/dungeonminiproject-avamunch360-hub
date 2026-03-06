8243b8a5bf120018c18a2f9600c5ecd4ac70a4d4
avamunch360-hub <ava.munch360@students.cmcss.net>
2026-03-06 07:31:40 -0600

    Update task5.py

---
+++ task5.py
@@ -0,0 +1,7 @@
+escaped = True
+
+if escaped:
+    outocme = "Legend: You step into freedom as the gates fade behind you."
+else:
+    outocme = "Doom: The dungeon claims another lost soul."
+print(outcome)
